1192ab90d58b395c1e3cd0076137c06b3d633503
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2017-08-22 00:18:50 -0500

    bounce off the walls

--- 2d-breakout.js
+++ 2d-breakout.js
@@ -2,12 +2,27 @@
 	
 	'use strict';
 
+	var ballRadius = 10;
 	var canvas = document.getElementById('2d-breakout-canvas');
 	var ctx = canvas.getContext('2d');
-	var x = canvas.width / 2;
-	var y = canvas.height - 30;
 	var dx = 2;
 	var dy = -2;
+	var x = canvas.width / 2;
+	var y = canvas.height - 30;
+
+	function ballCollisionDetection() {
+		
+		// ceiling and floor
+		if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+			dy = -dy;
+		}
+
+		// lefy and right walls
+		if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+			dx = -dx;
+		}
+
+	}
 
 	function clearCanvas() {
 		ctx.clearRect(0, 0, canvas.width, canvas.width);
@@ -16,11 +31,12 @@
 	function draw() {
 		clearCanvas();
 		drawBall();
+		ballCollisionDetection();
 	}
 
 	function drawBall() {
 		ctx.beginPath();
-		ctx.arc(x, y, 10, 0, Math.PI*2);
+		ctx.arc(x, y, ballRadius, 0, Math.PI*2);
 		ctx.fillStyle = '#0095DD';
 		ctx.fill();
 		ctx.closePath();
