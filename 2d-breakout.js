50c16c5f21b67c59b2a5f2ff3b3caf4fe48c0b3b
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2017-08-21 23:57:32 -0500

    Move the ball

--- 2d-breakout.js
+++ 2d-breakout.js
@@ -2,22 +2,35 @@
 	
 	'use strict';
 
-	let canvas = document.getElementById('2d-breakout-canvas');
-	let ctx = canvas.getContext('2d');
+	var canvas = document.getElementById('2d-breakout-canvas');
+	var ctx = canvas.getContext('2d');
+	var x = canvas.width / 2;
+	var y = canvas.height - 30;
+	var dx = 2;
+	var dy = -2;
 
-	ctx.beginPath();
-	ctx.rect(20, 40, 50, 60);
-	ctx.fillStyle = "#FF0000";
-	ctx.fill();
-	ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-	ctx.stroke();
-	ctx.closePath();
+	function clearCanvas() {
+		ctx.clearRect(0, 0, canvas.width, canvas.width);
+	}
 
-	ctx.beginPath();
-	ctx.arc(240, 180, 40, 0, Math.PI*2, false);
-	ctx.fillStyle = "green";
-	ctx.fill();
-	ctx.closePath();
+	function draw() {
+		clearCanvas();
+		drawBall();
+	}
+
+	function drawBall() {
+		ctx.beginPath();
+		ctx.arc(x, y, 10, 0, Math.PI*2);
+		ctx.fillStyle = '#0095DD';
+		ctx.fill();
+		ctx.closePath();
+		
+		// update ball coordinates
+		x += dx;
+		y += dy;
+	}
+	
+	setInterval(draw, 10);
 
 
 })();
